2a25146d3ab494660b99800c46d3bc1c03bc3f0d
liuyn0505 <liuyn0505@126.com>
2016-08-30 21:33:37 +0800

    My EasyBlog

---
+++ article/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from article.templates import *
+from datetime import datetime
+
+#def detail(request,my_args):
+ #   post=Article.objects.all()[int(my_args)]
+  #  str=("title=%s,category=%s,date_time=%s,content=%s"
+   #      %(post.title, post.category, post.date_time, post.content))
+    #return HttpResponse(str)
+def home(request):
+    post_list=Article.objects.all()
+    return render(request,'home.html',{'post_list':post_list})
